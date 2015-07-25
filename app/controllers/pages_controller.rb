bcf123ec1c437a9947887935695192316e646cc2
King601 <andrew@zerlex.net>
2015-07-24 23:53:33 -0500

    Intitial addition of posts + styling. Still needs cleanup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+		@posts = Post.all.limit(3).order("created_at desc")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :posts
+
   root 'pages#home'
   get 'pages/home'
 
