74e160b593459591a68a6d86e4e6d55dbb10b724
King601 <andrew@zerlex.net>
2015-07-25 00:24:24 -0500

    Add Friendly ID to Posts

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ gem "font-awesome-rails"
 gem 'will_paginate', '~> 3.0.7'
 gem 'pygments.rb', '~> 0.6.0'
 gem 'redcarpet', '~> 3.2.2'
+gem 'friendly_id', '~> 5.1.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
   validates_presence_of :title
   validates_presence_of :content
 end
