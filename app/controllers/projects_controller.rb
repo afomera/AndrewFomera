e1ef67a01bc772574543298cbf55d249819206a0
King601 <andrew@zerlex.net>
2015-07-25 17:25:34 -0500

    Add Projects to the site

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = Project.all.order("created_at DESC").paginate(page: params[:page], per_page: 2)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: "Your new project has been posted!"
+    else
+      render 'new', notice: "Sorry an error occurred. Please fix these and try again!"
+    end
+  end
+
+  def show
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Yeah! Great job! The project has been updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :description, :link)
+    end
+
+    def find_project
+      @project = Project.friendly.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :posts
+  resources :projects
   resources :contacts, only: [:new, :create], path: "contact"
   root 'pages#home'
   get 'pages/home'
