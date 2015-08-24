cf3e4199456b7b82a167097f93e21189ab5ed59a
King601 <andrew@zerlex.net>
2015-08-23 19:46:30 -0500

    Add Admin boolean, check if user is admin to allow to post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+    def check_admin_status
+      authenticate_user!
+      if current_user.admin
+        return
+      else
+        redirect_to root_path, notice: "Sorry, you do not have permission to do that."
+      end
+    end
 end
