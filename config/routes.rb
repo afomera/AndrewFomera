1d698faa577657521bd1839e602284ef88a3f69a
Andrew Fomera <andrew@zerlex.net>
2016-02-01 22:51:29 -0600

    Reorder routes by ABCs and use single quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, path: ''
+  
+  resources :contacts, only: [:new, :create], path: 'contact'
   resources :posts do
     member do
       patch :publish
     end
   end
   resources :projects
-  resources :contacts, only: [:new, :create], path: "contact"
   root 'pages#home'
-  get 'pages/home'
 
 end
