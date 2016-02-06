817dab5aa74a249524f8d0ec130acb342ca38b56
Andrew Fomera <andrew@zerlex.net>
2016-02-06 12:13:25 -0600

    Start adding OpenGraph stuff for Twitter to the site / posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def page_title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def meta_description(meta_description)
+    content_for(:meta_description) { meta_description }
+  end
+
   class HTMLwithPygments < Redcarpet::Render::HTML
     def block_code(code, language)
       Pygments.highlight(code, lexer: language)
