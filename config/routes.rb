895107cee81b29d4dc31fa283a211cf04d642083
Andrew Fomera <andrew@zerlex.net>
2017-01-29 21:47:11 -0600

    linting

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path: ''
-  
-  resources :contacts, only: [:new, :create], path: 'contact'
+
+  resources :contacts, only: %w(new create), path: 'contact'
+
   resources :posts do
     member do
       patch :publish
     end
   end
+
   resources :projects
   root 'pages#home'
-
 end
